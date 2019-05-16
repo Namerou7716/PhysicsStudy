b03183b4b4afa8a39e330f09d5fe7a0996ae433e
Namerou7716 <48110743+Namerou7716@users.noreply.github.com>
2019-05-16 14:29:00 +0900

    AddPlaneScript

--- Assets/AirPlaneScripts.cs
+++ Assets/AirPlaneScripts.cs
@@ -14,6 +14,11 @@ public class AirPlaneScripts : MonoBehaviour
         var left = transform.TransformVector(Vector3.left);
         var horizontal_left = new Vector3(left.x,0f,left.z).normalized;
         rb.AddTorque(Vector3.Cross(left, horizontal_left) * 2);
-
+        var forward = transform.TransformVector(Vector3.back);
+        var hori_forward = new Vector3(forward.x,0f,forward.z).normalized;
+        rb.AddTorque(Vector3.Cross(forward, Vector3.down));
+        var force = (rb.mass * rb.drag * target_kmph / 3.6f) / (1f - rb.drag * Time.fixedDeltaTime);
+        print(force);
+        rb.AddRelativeForce(new Vector3(0f, 0f, force));
     }
 }
