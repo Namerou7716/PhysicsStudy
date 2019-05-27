e1be761a60cf354d242829a42e0e136d584b2bf3
國分貞史 <48110743+Namerou7716@users.noreply.github.com>
2019-05-27 21:16:42 +0900

    makePhysics

---
+++ Assets/Scripts/BoundScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundScript : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        var rb = GetComponent<MyRigidbody>();
+       rb.AddForce(new Vector3(0, -9.8f, 0));
+    }
+}
---
+++ Assets/Scripts/MyRigidbody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRigidbody : MonoBehaviour
+{
+    public Vector3 acceleration;
+    public Vector3 velocity;
+    //public Vector3 COR;
+    public Vector3 position;
+    const float dt = 1 / 60f;
+    public void AddForce(Vector3 force)
+    {
+        acceleration += force;    
+    }
+    private void Start()
+    {
+        position = transform.position;
+    }
+    private void FixedUpdate()
+    {
+        velocity += acceleration * dt;
+        position += velocity * dt;
+        if (position.y < 0.5f)
+        {
+            velocity = -velocity;
+        }
+        transform.position = position;
+        acceleration = Vector3.zero;
+    }
+
+}
