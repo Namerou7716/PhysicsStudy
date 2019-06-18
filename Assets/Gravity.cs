65f9acec2edbb8134c559960bc0cecc2cb475f3d
國分貞史 <48110743+Namerou7716@users.noreply.github.com>
2019-06-18 16:05:02 +0900

    Implement bounds cube

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -5,30 +5,46 @@ using UnityEngine;
 public class Gravity : MonoBehaviour
 {
     float velocity = 0.0f;
+    float mass = 1;
+    [SerializeField]float damper;
+    float Force;
     float acceleration = 0.0f;
     float position = 0.0f;
     float gravity = -9.8f;
-    float reaction = 0.7f;
-    float dt = 1 / 60;
+    [SerializeField]float reaction;
+    float dt = 1f / 1200f;
     Vector3 mytransform;
+    Vector3 cubeLength;
     private void Start()
     {
         mytransform = this.GetComponent<Transform>().position;
+        cubeLength = this.GetComponent<Renderer>().bounds.size;
     }
     private void FixedUpdate()
     {
-            velocity += gravity *dt;
-            position += velocity *dt;
-            mytransform.y += position;
-
-            this.transform.position = mytransform;
-        
-        if (this.transform.position.y == 0.5f)
+        Force += -9.8f;
+        acceleration = Force;
+        velocity += acceleration * dt;
+        position = velocity *dt;
+        mytransform.y += position;
+        this.transform.position = mytransform;
+        if (this.transform.position.y < 0.5f)
+        {
+            //Force += (reaction * (cubeLength.y / 2 - this.transform.position.y)-damper*velocity);
+            //velocity = -velocity*0.8f;
+            float returnVelocity;
+            float Impulse;
+            returnVelocity = -velocity * 0.5f;
+            Impulse = returnVelocity - velocity;
+            print(Impulse);
+            Force += Impulse;
+        }
+       /* if (this.transform.position.y == 0.5f)
         {
             velocity *= -reaction;
             print(1);
         }
+        */
         //print(velocity);
-        //print(mytransform);
     }
 }
