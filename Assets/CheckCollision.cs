75de8158238cc396e40c5c5f0d76faa8a4ceae36
Namerou7716 <48110743+Namerou7716@users.noreply.github.com>
2019-06-20 01:19:11 +0900

    complete Euler

---
+++ Assets/CheckCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour
+{
+    public float time;
+    private void Update()
+    {
+        time += 1;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("Hit");
+            print(time);
+        }
+    }
+}
---
+++ Assets/SunEuler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunEuler : MonoBehaviour
+{
+    [SerializeField] float dt,checkTime;
+    Vector3 position,vector,acceleration;
+    float nextFrame=0;
+    CheckCollision check;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.transform.position;
+        vector = new Vector3(1f, 0, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextEuler();
+        //MyEuler();
+        if (GameObject.Find("Cube").GetComponent<CheckCollision>().time == checkTime+nextFrame)
+        {
+            print(this.transform.position);
+            nextFrame += 60;
+        }
+       // print(acceleration);
+    }
+    public void TextEuler()
+    {
+        position = this.transform.position;
+        //vector = vector - position* dt;
+        position = (position + vector*dt)/3600;
+        this.transform.position += position;
+        //vector = Vector3.zero;
+        position = Vector3.zero;
+    }
+    public void MyEuler()
+    {
+        position = this.transform.position;
+        //acceleration = vector / dt;
+       // vector = vector + acceleration*dt;
+        position = (position + vector*dt)/3600;
+        this.transform.position += position;
+        //vector = Vector3.zero;
+        acceleration = Vector3.zero;
+
+    }
+}
