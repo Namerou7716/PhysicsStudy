a1f21d0452a5204cc7dac6888b2cadf169d981e0
國分貞史 <48110743+Namerou7716@users.noreply.github.com>
2019-06-20 18:18:43 +0900

    Add Euler Vector

--- Assets/SunEuler.cs
+++ Assets/SunEuler.cs
@@ -6,45 +6,61 @@ public class SunEuler : MonoBehaviour
 {
     [SerializeField] float dt,checkTime;
     Vector3 position,vector,acceleration;
-    float nextFrame=0;
-    CheckCollision check;
+    float nextFrame = 0;
+    GameObject obj;
     // Start is called before the first frame update
     void Start()
     {
         position = this.transform.position;
         vector = new Vector3(1f, 0, 1f);
+        obj = GameObject.FindGameObjectWithTag("GameController");
     }
 
     // Update is called once per frame
     void Update()
     {
-        TextEuler();
-        //MyEuler();
-        if (GameObject.Find("Cube").GetComponent<CheckCollision>().time == checkTime+nextFrame)
+        //TextEuler();
+        MyEuler();
+        //VectorTextEuler();
+        //VectorMyEuler();
+        if (obj.GetComponent<CheckCollision>().time == checkTime+nextFrame)
         {
-            print(this.transform.position);
+            //print(this.transform.position);
             nextFrame += 60;
+            print(vector);
         }
        // print(acceleration);
     }
     public void TextEuler()
     {
         position = this.transform.position;
-        //vector = vector - position* dt;
         position = (position + vector*dt)/3600;
         this.transform.position += position;
-        //vector = Vector3.zero;
         position = Vector3.zero;
     }
     public void MyEuler()
     {
         position = this.transform.position;
-        //acceleration = vector / dt;
-       // vector = vector + acceleration*dt;
         position = (position + vector*dt)/3600;
         this.transform.position += position;
-        //vector = Vector3.zero;
-        acceleration = Vector3.zero;
-
+        //print(this.transform.position);
+    }
+    public void VectorTextEuler()
+    {
+        position = this.transform.position;
+        vector = vector - position * dt;
+        position += vector/3600;
+        //print(vector);
+        this.transform.position = position;
+    }
+    public void VectorMyEuler()
+    {
+        position = this.transform.position;
+        acceleration = vector / dt;
+        acceleration /= 3600;
+        vector = vector + acceleration * dt;
+        //print(vector);
+        position += vector/60;
+        this.transform.position = position;
     }
 }
